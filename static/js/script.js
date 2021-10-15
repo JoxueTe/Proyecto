b97d41864330c4a210da25141145bce03b54a4db
Brandon San Martin <bsanmartin@unicesar.edu.co>
2021-10-14 19:38:09 -0500

    añadiendo cambios de previsualizacion de imagen

--- static/js/script.js
+++ static/js/script.js
@@ -3,23 +3,26 @@ function validar_formulario(){
     var username= document.getElementById("nombre");
     var contraseña= document.getElementById("pass");
 
-    if(username.value.length==0 || username.value.length<8){
+
+    if (contraseña.value.length==0 || contraseña.value.length<4 ){
+        alert("Ingrese una cotraseña con mas de 3 digitos");
+        passid.focus();
+        return false
+    }
+
+    if(username.value.length==0 || username.value.length<5){
         // Swal.fire(
         //   'heading',
         //   'text',
         //   'success'
         // )
-        alert("Ingrese un nombre correctamente");
-        console.log("Enviando formulario")
+        alert("El nombre debe ser mayor de 4 caracteres");
+        console.log("Enviando formulario");
         passid.focus();
         return false;
     }
 
-    if (contraseña.value.length==0 || contraseña.value.length<3 ){
-        alert("Ingrese una cotraseña");
-        passid.focus();
-        return false;
-    }
+    
 
 }
 
@@ -32,4 +35,15 @@ function validar_correo(){
         return false;
     }
     
+}
+
+function send(){
+    var id=document.getElementById("id");
+    alert("entro")
+
+    if (id.value.length==0 || id.value.length<3){
+        alert("el codigo debe tener almenos 3 digitos");
+        passid.focus();
+        return false;
+    }
 }
