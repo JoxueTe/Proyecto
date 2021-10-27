0c072a358ef0592da317464cb6f1a3a46b703334
Brandon San Martin <bsanmartin@unicesar.edu.co>
2021-10-27 13:27:37 -0500

    Mensaje eliminar de proveedores

--- database/database_controller.py
+++ database/database_controller.py
@@ -47,8 +47,8 @@ def get_all_usuario():
 def insert_proveedor(id,nombre,direccion,email,imagen,fregistro,ciudad,telefono,descripcion):
     db = get_db()
     cursor = db.cursor()
-    statement= "INSERT INTO usuario(id_prov,nom_prov,dirección_prov,email_prov,img_prov,fecha_ingreso_prov,ciudad_prov,tel_prov,desc_prov) VALUES(?,?,?,?,?,?,?,?,?)"
-    cursor.execute(statement,[id,nombre,direccion,email,imagen,fregistro,ciudad,telefono,descripcion])
+    statement= "INSERT INTO proveedor(id_prov,nom_prov,dirección_prov,email_prov,fecha_ingreso_prov,img_prov,ciudad_prov,tel_prov,desc_prov) VALUES(?,?,?,?,?,?,?,?,?)"
+    cursor.execute(statement,[id,nombre,direccion,email,fregistro,imagen,ciudad,telefono,descripcion])
     db.commit()
     db.close()
     return True
@@ -64,7 +64,7 @@ def edit_proveedor(id,nombre,usuario,contraseña,email,imagen,fingreso,rol,telef
 def dell_proveedor(id):
     db = get_db()
     cursor = db.cursor()
-    statement= "DELETE FROM usuario WHERE identif_usua=?;"
+    statement= "DELETE FROM proveedor WHERE id_prov=?;"
     cursor.execute(statement,[id])
     db.commit()
     db.close()
@@ -73,13 +73,56 @@ def dell_proveedor(id):
 def get_one_proveedor(id):
     db = get_db()
     cursor = db.cursor()
-    statement= "SELECT identif_usua,nom_usua, usuari_usua, passw_usua, email_usua, img_usua, fecha_ingreso_usua, rol_usua, tel_usua FROM usuario WHERE identif_usua=?;"
+    statement= "SELECT id_prov,nom_prov,dirección_prov,email_prov,fecha_ingreso_prov,img_prov,ciudad_prov,desc_prov,tel_prov FROM proveedor WHERE id_prov=?;"
     cursor.execute(statement,[id])
     return cursor.fetchone()
 
 def get_all_proveedor():
     db = get_db()
     cursor = db.cursor()
-    query= "SELECT identif_usua,nom_usua, usuari_usua, passw_usua, email_usua, img_usua, fecha_ingreso_usua, rol_usua, tel_usua FROM usuario;"
+    query= "SELECT id_prov,nom_prov,dirección_prov,email_prov,fecha_ingreso_prov,img_prov,ciudad_prov,desc_prov,tel_prov FROM proveedor;"
     cursor.execute(query)
     return cursor.fetchall()
+
+#---------------------------------------------------------------------CRUD PRODUCTO---------------------------------------------
+
+def insert_producto(id,nombre,direccion,email,imagen,fregistro,ciudad,telefono,descripcion):
+    db = get_db()
+    cursor = db.cursor()
+    statement= "INSERT INTO producto(id_prod,nom_prod,cant_min_prod,cant_max_prod, desc_prod,img_prod,fecha_ing_prod,cant_prod) VALUES(?,?,?,?,?,?,?,?,?)"
+    cursor.execute(statement,[id,nombre,direccion,email,fregistro,imagen,ciudad,telefono,descripcion])
+    db.commit()
+    db.close()
+    return True
+
+def edit_producto(id,nombre,usuario,contraseña,email,imagen,fingreso,rol,telefono):
+    db = get_db()
+    cursor = db.cursor()
+    statement= "UPDATE usuario SET nom_usua = ?, usuari_usua = ?, passw_usua = ?, email_usua = ?, img_usua = ?, fecha_ingreso_usua = ?, rol_usua = ?, tel_usua = ? WHERE identif_usua = ?;"
+    cursor.execute(statement,[nombre,usuario,contraseña,email,imagen,fingreso,rol,telefono,id])
+    db.commit()
+    return True
+
+def dell_producto(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement= "DELETE FROM proveedor WHERE id_prov=?;"
+    cursor.execute(statement,[id])
+    db.commit()
+    db.close()
+    return True
+
+def get_one_producto(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement= "SELECT id_prov,nom_prov,dirección_prov,email_prov,fecha_ingreso_prov,img_prov,ciudad_prov,desc_prov,tel_prov FROM proveedor WHERE id_prov=?;"
+    cursor.execute(statement,[id])
+    return cursor.fetchone()
+
+def get_all_producto():
+    db = get_db()
+    cursor = db.cursor()
+    query= "SELECT id_prov,nom_prov,dirección_prov,email_prov,fecha_ingreso_prov,img_prov,ciudad_prov,desc_prov,tel_prov FROM proveedor;"
+    cursor.execute(query)
+    return cursor.fetchall()
+
